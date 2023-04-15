a6b58dafd3e19c2fbea1b6ba69a0224d5515ff1b
lksad <it20605902@my.sliit.lk>
2023-04-15 13:37:48 +0530

    add all store components

--- src/App.js
+++ src/App.js
@@ -14,12 +14,17 @@ import Login from "./components/Users/Login";
 import Register from "./components/Users/Register";
 
 //store imports
-import StoreHome from "./components/Store/StoreHome";
-import StoreAddProductForm from "./components/Store/StoreAddProductForm";
+import StorePaymentScreen from "./components/Store/StorePaymentScreen";
 import StoreAdminProductsEdit from "./components/Store/StoreAdminProductsEdit";
-
-//order imports
+import StoreAdminProducts from "./components/Store/StoreAdminProducts";
+import StoreAdminOrders from "./components/Store/StoreAdminOrders";
+import StoreAdminPayments from "./components/Store/StoreAdminPayments";
+import StoreHome from "./components/Store/StoreHome";
+import StoreProducts from "./components/Store/StoreProducts";
+import StoreProductsDetails from "./components/Store/StoreProductsDetails";
 import StoreOrderForm from "./components/Store/StoreOrderForm";
+import StoreAddProductForm from "./components/Store/StoreAddProductForm";
+
 
 function App() {
   useEffect(() => {}, []);
@@ -33,19 +38,38 @@ function App() {
       
 
         <Route path="/store" element={<StoreHome />} />
+        <Route path="/store/products/:category" element={<StoreProducts />} />
+        <Route
+          path="/store/products/product/:id"
+          element={<StoreProductsDetails />}
+        />
+        <Route
+          path="/store/order/store-order-create/:product/:quantity/:price"
+          element={<StoreOrderForm />}
+        />
+        <Route
+          path="/store/order/payment/:orderId"
+          element={<StorePaymentScreen />}
+        />
         <Route
           path="/store/product/add-product"
           element={<StoreAddProductForm />}
         />
-
+        <Route
+          path="/store/store-admin-products"
+          element={<StoreAdminProducts />}
+        />
+        <Route
+          path="/store/store-admin-orders"
+          element={<StoreAdminOrders />}
+        />
+        <Route
+          path="/store/store-admin-payments"
+          element={<StoreAdminPayments />}
+        />
         <Route
           path="/store/store-admin-products/edit/:pid"
           element={<StoreAdminProductsEdit />}
-        />
-
-        <Route
-          path="/store/order/store-order-create/:product/:quantity/:price"
-          element={<StoreOrderForm />}
         />
 
       </Routes>
