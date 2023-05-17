a77afd3edee978958488b3907ffdcc2b1fbb77eb
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-05-17 22:11:21 +0530

    fix unused usings

--- src/components/Store/StoreAdminCommision.jsx
+++ src/components/Store/StoreAdminCommision.jsx
@@ -1,24 +1,18 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import swal from "sweetalert";
 import "./Store.css";
 import { exportToCSV } from "../utils";
-
 
 const StoreAdminCommision = () => {
   const [commisions, setCommisions] = useState([]);
   //must check endpoint
 
   useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_API}/orders/commisions`)
-      .then((res) => {
-        setCommisions(res.data);
-      });
+    axios.get(`${process.env.REACT_APP_API}/orders/commisions`).then((res) => {
+      setCommisions(res.data);
+    });
   }, []);
-
-  
 
   return (
     <>
@@ -29,41 +23,39 @@ const StoreAdminCommision = () => {
         className="h-100 store-container d-flex justify-content-center p-5"
       >
         <div className=" w-100" id="store-admin-admin456412123">
-        
           <p> These are the products exists inside the store </p>
 
-          <div className="d-flex">
-        
-        </div>
+          <div className="d-flex"></div>
 
           <div className="d-flex">
-            <Link to="/store/product/add-product" style={{marginRight:"30px"}}>
+            <Link
+              to="/store/product/add-product"
+              style={{ marginRight: "30px" }}
+            >
               <button className="btn btn-success">
                 <i class="fa-solid fa-plus mx-2"></i> Add Product to Store
               </button>
             </Link>
 
             <button
-            onClick={() => exportToCSV(commisions, "Products")}
-            type="button"
-            class="btn btn-primary"
-          >
-            Download Report
-          </button>
+              onClick={() => exportToCSV(commisions, "Products")}
+              type="button"
+              class="btn btn-primary"
+            >
+              Download Report
+            </button>
           </div>
 
           <table
             class="table mt-4 store-orders-container pdfdiv"
             id="app-store-admin-table-header-44512135"
-           
           >
             <thead className="store-admin-table-header">
               <tr>
-               <th scope="col">#</th>
+                <th scope="col">#</th>
                 <th scope="col">Comision ID</th>
                 <th scope="col">Order ID</th>
                 <th scope="col">Comision</th>
-               
               </tr>
             </thead>
             <tbody>
@@ -71,12 +63,8 @@ const StoreAdminCommision = () => {
                 commisions.map((com, index) => (
                   <tr key={index}>
                     <th scope="row">{index + 1}</th>
-                    <td> 
-                      {com.id}
-                    </td>
-                    <td>
-                      {com.orderId}
-                    </td>
+                    <td>{com.id}</td>
+                    <td>{com.orderId}</td>
                     <td>{com.commision}</td>
                   </tr>
                 ))}
