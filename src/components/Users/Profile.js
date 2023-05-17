6491a30b127287a78aae99629fe7476659ffd193
lksad <it20605902@my.sliit.lk>
2023-05-17 21:16:56 +0530

    current user component added

--- src/components/Users/Profile.js
+++ src/components/Users/Profile.js
@@ -1,75 +1,44 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-
-
+import "../Store/Store.css";
+import { exportToCSV } from "../utils";
 
 const Profile = () => {
+  const [currentUsers, setCurrentUser] = useState([]);
 
-    const [user, setUser] = useState([]);
-
-    useEffect(() => {
-        axios
-          .get(`${process.env.REACT_APP_API}/orders/current-user`)
-          .then((res) => {
-            setUser(res.data);
-          });
-      }, []);
-  
+  //must check endpoint
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API}/orders/current-user`)
+      .then((res) => {
+        setCurrentUser(res.data);
+      });
+  }, []);
 
   return (
-    <section class="vh-100" style={{backgroundColor: "#f4f5f7"}}>
-    <div class="container py-5 h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col col-lg-6 mb-4 mb-lg-0">
-          <div class="card mb-3" style={{borderRadius: ".5rem"}}>
-            <div class="row g-0">
-              <div class="col-md-4 gradient-custom text-center text-white"
-                 style={{borderRadius:".5rem",}}>
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
-                  alt="Avatar" class="img-fluid my-5" style={{width: "80px"}} />
-                <h5>Marie Horwitz</h5>
-                <p>Web Designer</p>
-                <i class="far fa-edit mb-5"></i>
-              </div>
-              <div class="col-md-8">
-                <div class="card-body p-4">
-                  <h6>Information</h6>
-                  <hr class="mt-0 mb-4"/>
-                  <div class="row pt-1">
-                    <div class="col-6 mb-3">
-                      <h6>Email</h6>
-                      <p class="text-muted">info@example.com</p>
-                    </div>
-                    <div class="col-6 mb-3">
-                      <h6>Phone</h6>
-                      <p class="text-muted">123 456 789</p>
-                    </div>
-                  </div>
-                  <h6>Projects</h6>
-                  <hr class="mt-0 mb-4"/>
-                  <div class="row pt-1">
-                    <div class="col-6 mb-3">
-                      <h6>Recent</h6>
-                      <p class="text-muted">Lorem ipsum</p>
-                    </div>
-                    <div class="col-6 mb-3">
-                      <h6>Most Viewed</h6>
-                      <p class="text-muted">Dolor sit amet</p>
-                    </div>
-                  </div>
-                  <div class="d-flex justify-content-start">
-                    <a href="#!"><i class="fab fa-facebook-f fa-lg me-3"></i></a>
-                    <a href="#!"><i class="fab fa-twitter fa-lg me-3"></i></a>
-                    <a href="#!"><i class="fab fa-instagram fa-lg"></i></a>
-                  </div>
-                </div>
-              </div>
+    <div className="store-container d-flex justify-content-center p-5">
+      <div>
+      {currentUsers.map((data) => (
+        <div key={data.id}>
+          <h3>Order ID: {data.id}</h3>
+          <p>User ID: {data.userId}</p>
+          <p>Status: {data.status}</p>
+          <h4>Products:</h4>
+          {data.products.map((item) => (
+            <div key={item.product.id}>
+              <p>Name: {item.product.name}</p>
+              <p>Description: {item.product.description}</p>
+              <p>Quantity: {item.quantity}</p>
+              <p>Price: {item.product.price}</p>
+              <p>Buy Price: {item.product.buyPrice}</p>
             </div>
-          </div>
+          ))}
+          <p>Total Price: {data.totalPrice}</p>
+          <hr />
         </div>
-      </div>
+      ))}
     </div>
-  </section>
+    </div>
   );
 };
 
