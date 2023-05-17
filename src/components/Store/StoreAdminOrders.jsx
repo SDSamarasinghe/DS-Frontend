61f906a416b7f1048b2e4029838f33cbfeaeb961
lksad <it20605902@my.sliit.lk>
2023-05-17 11:19:34 +0530

    add excel sheet to generate report in orders

--- src/components/Store/StoreAdminOrders.jsx
+++ src/components/Store/StoreAdminOrders.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./Store.css";
-import html2canvas from "html2canvas";
-import jsPDF from "jspdf";
+import { exportToCSV } from "../utils";
 
 const StoreAdminOrders = () => {
   const [orders, setOrders] = useState([]);
@@ -16,72 +15,6 @@ const StoreAdminOrders = () => {
       });
   }, []);
 
-  const printPdf = () => {
-    const input = document.querySelector(".pdfdiv");
-    html2canvas(input).then((canvas) => {
-      // var img = new Image();
-      const doc = new jsPDF("p", "mm", "a4");
-      doc.setTextColor(255, 0, 0);
-      doc.setFontSize(28);
-      doc.setTextColor(0, 0, 255);
-      doc.setFontSize(16);
-      doc.text(10, 70, "Agrotec LLC");
-      doc.setTextColor(0, 255, 0);
-      doc.setFontSize(12);
-      doc.text(145, 85, "Signature :");
-      //Date
-      // eslint-disable-next-line no-array-constructor
-      var m_names = new Array(
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-      );
-
-      var today = new Date();
-      var seconds = today.getSeconds();
-      var minutes = today.getMinutes();
-      var hours = today.getHours();
-      var curr_date = today.getDate();
-      var curr_month = today.getMonth();
-      var curr_year = today.getFullYear();
-
-      today =
-        m_names[curr_month] +
-        " " +
-        curr_date +
-        "/ " +
-        curr_year +
-        " | " +
-        hours +
-        "h : " +
-        minutes +
-        "min : " +
-        seconds +
-        "sec";
-      var newdat = today;
-      doc.setTextColor(0, 0, 0);
-      doc.setFontSize(11);
-      doc.text(130, 93, newdat);
-      var imgHeight = (canvas.height * 200) / canvas.width;
-      const imgData = canvas.toDataURL("image/png");
-      doc.addImage(imgData, "JPEG", 5, 100, 200, imgHeight);
-      const date = Date().split(" ");
-      // we use a date string to generate our filename.
-      const dateStr =
-        "Florage Reports" + date[0] + date[1] + date[2] + date[3] + date[4];
-      doc.save(`report_${dateStr}.pdf`);
-    });
-  };
-
   return (
     <div className="store-container d-flex justify-content-center p-5">
       <div className="min-vh-100 w-100" id="store-admin-admin456412123">
@@ -89,8 +22,12 @@ const StoreAdminOrders = () => {
         <p> These are the orders recived inside this month </p>
 
         <div className="d-flex">
-          <button className="btn btn-success" onClick={printPdf} style={{backgroundColor:"#1c305c"}}>
-            <i class="fa-solid fa-file-pdf mx-2"></i> Download Orders As PDF
+        <button
+            onClick={() => exportToCSV(orders, "Orders")}
+            type="button"
+            class="h-10 focus:outline-none text-white bg-blue-900 hover:bg-orange-600  font-medium rounded text-sm px-5 py-2.5"
+          >
+            Download Report
           </button>
         </div>
 
